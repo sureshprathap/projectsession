bb06788bad40e80011157ff4f07f13b7b0f8c576
sureshprathap <92008539+sureshprathap@users.noreply.github.com>
2021-10-11 14:02:56 +0530

    Sample file for file sequentials
    File sequential sample code for the project

---
+++ CreditInquiryTest.java
@@ -0,0 +1,12 @@
+package sample;
+
+//This program tests class CreditInquiry.
+
+public class CreditInquiryTest
+{  
+public static void main( String args[] )
+{
+   CreditInquiry application = new CreditInquiry();
+   application.processRequests();
+} // end main
+} // end class CreditInquiryTest
---
+++ MenuOption.java
@@ -0,0 +1,24 @@
+package sample;
+
+//Defines an enum type for the credit inquiry program's options.
+
+public enum MenuOption
+{
+// declare contents of enum type
+ZERO_BALANCE( 1 ),
+CREDIT_BALANCE( 2 ),
+DEBIT_BALANCE( 3 ),
+END( 4 );
+
+private final int value; // current menu option
+
+MenuOption( int valueOption )
+{
+   value = valueOption;
+} // end MenuOptions enum constructor
+
+public int getValue()
+{
+   return value;
+} // end method getValue
+} // end enum MenuOption
